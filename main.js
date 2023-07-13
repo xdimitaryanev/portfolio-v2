32a9a679b958eb16ad50e034ab776af1f8b26ac9
xdimitaryanev <118223301+xdimitaryanev@users.noreply.github.com>
2023-07-13 17:18:56 +0300

    update

--- main.js
+++ main.js
@@ -2,6 +2,8 @@
 import './styles/normalize.css'
 import './styles/style.css'
 import './styles/components/header.css'
+import './styles/components/hero.css'
+import './styles/components/about.css'
 import './styles/utils.css'
 
 
