e882ffa4d03ad41b72c14f8f58ba64911c5821ce
xdimitaryanev <118223301+xdimitaryanev@users.noreply.github.com>
2023-07-14 17:47:08 +0300

    update

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ import './styles/components/header.css'
 import './styles/components/hero.css'
 import './styles/components/about.css'
 import './styles/components/work.css'
+import './styles/components/contact.css'
 import './styles/utils.css'
 
 
